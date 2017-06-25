f4a96d78391056c8d6915f64766b1ad28cad48fa
DoubleBite <r05921030@ntu.edu.tw>
2017-06-25 14:31:06 +0800

    tmp

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
-nvcc -std=c++11 --expt-extended-lambda -arch=sm_30 task.cu utility.cu parallel_louvain.cu louvain_utils.cu -o main.out &>log.log
-./main.out dataset/web-Stanford.txt
+nvcc -std=c++11 --expt-extended-lambda -arch=sm_30 task.cu utility.cu parallel_louvain.cu louvain_utils.cu -o main.out &>compile.log
+./main.out dataset/email-Eu-core.txt 
